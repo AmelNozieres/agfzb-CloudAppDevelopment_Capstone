97bfd8dc62c76c09624343ceb7aed9b735ad925d
AmelNozieres <amel.nozieres@gmail.com>
2023-04-12 09:37:50 -0400

    lab modif postgre

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+
+
+
+
+
+
+# Register your models here.
+
 class CarModelInline(admin.StackedInline):
     model = CarModel
     extra = 5
@@ -12,13 +20,3 @@ class CarMakeAdmin(admin.ModelAdmin):
 
 admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel)
-
-# Register your models here.
-
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -81,10 +81,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'amelnozieres',
+        'PASSWORD': 'MjI2MjUtYW1lbG5v',
+        'HOST' : '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
-
+#SECRET_KEY = "MjI2MjUtYW1lbG5v"
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
