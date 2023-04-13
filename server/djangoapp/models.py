430f5ee35ce3841a523a4503232a7b0954506764
AmelNozieres <amel.nozieres@gmail.com>
2023-04-13 17:13:27 +0200

    week3-proxy

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -11,7 +11,7 @@ from django.utils.timezone import now
 # - __str__ method to print a car make object
 class CarMake(models.Model):
     name = models.CharField(null= False, max_length=30, default='Audi')
-    description = models.CharField(null= False, max_length=300, default='Audi cars are aight')
+    description = models.CharField(null= False, max_length=300, default='Audi cars are cool')
 
     def __str__(self):
         return 'Name:' + self.name + ',' + \
@@ -27,22 +27,74 @@ class CarMake(models.Model):
 # - __str__ method to print a car make object
 
 class CarModel(models.Model):
-    SEDAN = 'sedan'
-    SUV = 'suv'
-    WAGON = 'wagon'
-    OTHERS = 'others'
-    CAR_CHOICES = [(SEDAN, "Sedan"), (SUV, 'SUV'), (WAGON, 'Wagon'), (OTHERS, 'Others')]
-    carmake = models.ForeignKey(CarMake, null= True, on_delete=models.CASCADE)
-    name = models.CharField(null= False, max_length=30, default='Audi X8')
-    dealerid = models.IntegerField(null=True)
-    cartype = models.CharField(null= False, max_length=20, choices= CAR_CHOICES, default=SEDAN)
-    year = models.DateField(null= True)
+    id = models.IntegerField(default=1,primary_key=True)
+    name = models.CharField(null=False, max_length=100, default='Car')
+   
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    MINIVAN = 'Minivan'
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (MINIVAN, 'Minivan')
+    ]
+
+    type = models.CharField(
+        null=False,
+        max_length=50,
+        choices=CAR_TYPES,
+        default=SEDAN
+    )
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    year = models.DateField(default=now)
 
     def __str__(self):
-        return 'Name ' + self.name
-
+        return "Name: " + self.name
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
 
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st,state, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        self.state = state
+        # Dealer zip
+        self.zip = zip
+        self.idx = 0
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+
+class DealerReview:
+    def __init__(self, name, id, dealership, review, purchase, purchase_date, car_make, car_model, car_year, sentiment):
+        self.name = name
+        self.id = id,
+        self.dealership = dealership
+        self.review = review
+        self.purchase = purchase
+        self.purchase_date = purchase_date
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year = car_year
+        self.sentiment = sentiment
+
+    def __str__(self):
+        return "Review: " + self.review
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,7 +1,10 @@
 import requests
 import json
-# import related models here
+from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions
 
 
 # Create a `get_request` to make HTTP GET requests
@@ -32,3 +35,161 @@ from requests.auth import HTTPBasicAuth
 
 
 
+
+def get_request(url, **kwargs):
+    print(kwargs)
+    api_key = kwargs.get("api_key")
+    print("GET from {} ".format(url))
+    try:
+         # Basic authentication GET
+        if api_key:
+            params = dict()
+            params["text"] = kwargs["text"]
+            params["version"] = kwargs["version"]
+            params["features"] = kwargs["features"]
+            params["return_analyzed_text"] = kwargs["return_analyzed_text"]
+            response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
+                                    auth=HTTPBasicAuth('apikey', api_key))
+        else:
+            # no authentication GET
+            response = requests.get(url, headers={'Content-Type': 'application/json'}, params=kwargs)
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
+
+def post_request(url, payload, **kwargs):
+    print('**********post_reuqest**********')
+    print(kwargs)
+    print("POST to {} ".format(url))
+    print(payload)
+    response = requests.post(url, params=kwargs, json=payload)
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    # json_data = json.loads(response.text)
+    # return json_data
+    json_obj = payload["review"]
+    print(kwargs)
+    try:
+        response = requests.post(url, json=json_obj, params=kwargs)
+    except:
+        print("Something went wrong")
+    print (response)
+    return response
+
+def get_dealers_from_cf(url):
+    results = []
+    json_result = get_request(url)
+    if json_result:
+        dealers = json_result
+        for dealer in dealers:
+            dealer_doc = dealer["doc"]
+            dealer_obj = CarDealer(
+                address=dealer_doc["address"],
+                city=dealer_doc["city"],
+                full_name=dealer_doc["full_name"],
+                id=dealer_doc["id"],
+                lat=dealer_doc["lat"],
+                long=dealer_doc["long"],
+                short_name=dealer_doc["short_name"],
+                st=dealer_doc["st"],
+                state = dealer_doc["state"],
+                zip=dealer_doc["zip"]
+            )
+            results.append(dealer_obj)
+    return results
+
+
+def get_dealer_by_id_from_cf(url, dealer_id):
+    json_result = get_request(url, id=dealer_id)
+    print('------------------')
+    print(json_result[0])
+    if json_result[0]:
+        dealer_doc = json_result[0]
+        dealer_obj = CarDealer(
+            address=dealer_doc["address"],
+            city=dealer_doc["city"],
+            full_name=dealer_doc["full_name"],
+            id=dealer_doc["id"],
+            lat=dealer_doc["lat"],
+            long=dealer_doc["long"],
+            short_name=dealer_doc["short_name"],
+            st=dealer_doc["st"],
+            state= dealer_doc["state"],
+            zip=dealer_doc["zip"]
+        )
+    return dealer_obj
+
+
+def get_dealers_by_st_from_cf(url, state):
+    results = []
+    json_result = get_request(url, st=state)
+    if json_result:
+        dealers = json_result["body"]
+        for dealer_doc in dealers:
+            dealer_obj = CarDealer(
+                address=dealer_doc["address"],
+                city=dealer_doc["city"],
+                full_name=dealer_doc["full_name"],
+                id=dealer_doc["id"],
+                lat=dealer_doc["lat"],
+                long=dealer_doc["long"],
+                short_name=dealer_doc["short_name"],
+                st=dealer_doc["st"],
+                state= dealer_doc["state"],
+                zip=dealer_doc["zip"]
+            )
+            results.append(dealer_obj)
+    return results
+
+
+def get_dealer_reviews_from_cf(url, dealerId):
+    results = []
+    print("get_dealer_reviews_from_cf********************")
+    
+    json_result = get_request(url, id=dealerId)
+    print("json_result")
+    print(json_result)
+    if json_result:
+        reviews = json_result["data"]["docs"]
+
+        for dealer_review in reviews:
+            print("************dealer_review********************")
+            print(dealer_review["review"])
+            
+            review_obj = DealerReview(dealership=dealer_review["dealership"],
+                                      car_make= dealer_review["car_make"],
+                                      car_model= dealer_review["car_model"],
+                                      car_year= dealer_review["car_year"],
+                                      name=dealer_review["name"],
+                                      purchase=dealer_review["purchase"],
+                                      purchase_date=dealer_review["purchase_date"],
+                                      id= dealer_review["id"],
+                                      review=dealer_review["review"],
+                                      sentiment= "none")
+
+            
+            sentiment = analyze_review_sentiments(review_obj.review)
+            print(sentiment)
+            review_obj.sentiment = sentiment
+            results.append(review_obj)
+
+    return results
+
+def analyze_review_sentiments(text):
+    url = "https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/3817681f-fedc-46b5-b31b-b25922b49880"
+    api_key = "CQ6GiMAYhTEf6sf_4GGqPBsedvl2OQjFiOKDBCE6GPTe"
+    authenticator = IAMAuthenticator(api_key)
+    natural_language_understanding = NaturalLanguageUnderstandingV1(version='2022-08-01',authenticator=authenticator)
+    natural_language_understanding.set_service_url(url)
+    response = natural_language_understanding.analyze( text=text+"hello hello hello",features=Features(sentiment=SentimentOptions(targets=[text+"hello hello hello"]))).get_result() 
+    
+    print("json.dumps(response)")
+    print(json.dumps(response))
+    label=json.dumps(response, indent=2)
+    label = response['sentiment']['document']['label']
+    
+    return(label)
